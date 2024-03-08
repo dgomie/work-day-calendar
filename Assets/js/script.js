840d3cdaf3a17477a42a3b138b59f546e00a2c6c
Danny <danny.f.gomez@gmail.com>
2024-03-08 11:32:09 -0500

    Debug and refactor getDate function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,13 +2,34 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  var currentDay = $("#currentDay");
+  var today = dayjs();
+
+  function getDate() {
+    var todayDay = today.format("DD");
+    var todayLastDigit = todayDay.split('')[1];
+    var suffix = "";
+    // Determines the suffix needed based on
+    if (todayLastDigit === "1") {
+      suffix = "st";
+    } else if (todayLastDigit === "2") {
+      suffix = "nd";
+    } else if (todayLastDigit === "3") {
+      suffix = "rd";
+    } else {
+      suffix = "th";
+    }
+    var todayDate = today.format(`dddd, MMMM D[${suffix}]`);
+    return todayDate;
+  }
+  
+  // change currentDay element by calling getDate function.
+  currentDay.text(getDate());
+
+
+  /* TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
+   */
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
