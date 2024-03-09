e9928191b242d53e8dec83be3df9fe21940ff02d
Danny <danny.f.gomez@gmail.com>
2024-03-08 19:48:31 -0500

    Add checkHour function to determine if hour block in past,present, or future.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,7 @@
 $(function () {
   var currentDay = $("#currentDay");
   var today = dayjs();
+  console.log(today.format('H'))
 
   function getDate() {
     var todayDay = today.format("DD");
@@ -28,44 +29,71 @@ $(function () {
 
   function createHourBlocks() {
     // loops for the set amount of hours in workday and creates a time block. Default sets block to future time
-    for (let i = 0; i < 9; i++) {
-    var hourBlock = $('<div>', {id: `hour-${i}`, 'class': 'row time-block future'});
-    var hourDisplay = $('<div>', {'class': 'col-2 col-md-1 hour text-center py-3'});
-    var textDisplay = $('<textarea>', {'class': 'col-8 col-md-10 description', 'rows': '3'});
-    var saveButton = $('<button>', {'class': 'btn saveBtn col-2 col-md-1', 'aria-label': 'save'})
-    var saveIcon = $('<i>', {'class': 'fas fa-save', 'aria-hidden': 'true'})
     
-    // Time display. Converts the 24-hour clock to 12-hour and determines if AM or PM
-    var hour = i + 9
-    var amPM = 'AM'
-    if (hour === 12){
-      amPM = 'PM'
-    } else if (hour > 12) {
-      hour %= 12
-      amPM = 'PM'
-    }
-    // console.log('hour: '+ hour+amPM)
-    hourDisplay.text(hour + amPM)
-    // appends created hour block elements to the hour block container. renders the content in main container
-    $(saveButton).append(saveIcon)
-    $(hourBlock).append(hourDisplay, textDisplay, saveButton)
-    $('#container').append(hourBlock)
+    for (let i = 0; i < 9; i++) {
+      var hour = i + 9;
+      var blockColor = checkHour(hour);
+      var hourBlock = $("<div>", {
+        id: `hour-${hour}`,
+        class: blockColor,
+      });
+      var hourDisplay = $("<div>", {
+        class: "col-2 col-md-1 hour text-center py-3",
+      });
+      var textDisplay = $("<textarea>", {
+        class: "col-8 col-md-10 description",
+        rows: "3",
+      });
+      var saveButton = $("<button>", {
+        class: "btn saveBtn col-2 col-md-1",
+        "aria-label": "save",
+      });
+      // saveButton.click(function() {
+      //   console.log(this)
+      // });
+      var saveIcon = $("<i>", { class: "fas fa-save", "aria-hidden": "true" });
+
+      // Time display. Converts the 24-hour clock to 12-hour and determines if AM or PM
+      var amPM = "AM";
+      if (hour === 12) {
+        amPM = "PM";
+      } else if (hour > 12) {
+        hour %= 12;
+        amPM = "PM";
+      }
+      // console.log('hour: '+ hour+amPM)
+      hourDisplay.text(hour + amPM);
+      // appends created hour block elements to the hour block container. renders the content in main container
+      $(saveButton).append(saveIcon);
+      $(hourBlock).append(hourDisplay, textDisplay, saveButton);
+      $("#container").append(hourBlock);
     }
   }
 
-  createHourBlocks()
+  createHourBlocks();
 
   /* TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
    */
 
-  function saveEvent() {}
+  // function saveText() {}
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
+  function checkHour(hourArg) {
+    var currentHour = Number(today.format('H'));
+    // var currentHour = Number("");
+    if (hourArg < currentHour) {
+      return "row time-block past"
+    } else if (hourArg === currentHour){
+      return "row time-block present";
+    } else {
+      return "row time-block future";
+    }
+  };
+
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
